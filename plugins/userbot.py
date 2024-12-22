9ca5a6273968184364b9a025a734bc2478db3b53
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-12-22 10:20:58 +0530

    Update userbot.py

--- plugins/userbot.py
+++ plugins/userbot.py
@@ -8,7 +8,7 @@ from pyrogram.types import ChatMemberUpdated
 @app.on_chat_member_updated()
 async def on_bot_added(client: Client, chat_member_update: ChatMemberUpdated):
     # Check if the bot was added to the group
-    if chat_member_update.new_chat_member.is_bot and chat_member_update.new_chat_member.status == "member":
+    if chat_member_update.new_chat_member and chat_member_update.new_chat_member.user.id == client.me.id:
         chat_id = chat_member_update.chat.id
         
         # Wait for 10 seconds before sending the command
